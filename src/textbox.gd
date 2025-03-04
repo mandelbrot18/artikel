46140f57dc735c5e09350d44bec04cf49976253e
Leonie <leoniestiefenhofer@gmx.de>
2025-03-04 15:40:50 +0100

    functional textbox with text buffer and working text animation

--- src/textbox.gd
+++ src/textbox.gd
@@ -2,11 +2,12 @@ extends CanvasLayer
 
 @onready var textbox_container = $TextboxContainer
 @onready var label = $TextboxContainer/LabelContainer/Label
-@onready var end_symbol = $TextboxContainer/LabelContainer/End
 
+#tween is manipulated by 
 @onready var tween = get_tree().create_tween()
 
 const CHAR_READ_RATE = 0.05
+var dialogue_queue = []
 
 enum State {
 	READY, 
@@ -16,25 +17,72 @@ enum State {
 
 var current_state = State.READY
 
-func _ready() -> void:
-	print("starting state: ready")
-	#add_text("Es sitzt ein Vogel auf dem Leim.")
-	add_text("es sitzt ein vogel auf dem leim er flattert sehr doch kommt nicht heim ein schwarzer kater kommt herrzu die krallen scharf die augen gluh am baum hinauf und immer höher kommt er dem armen vogel näher. der vogel spricht weil das so ist und weil mich doch der kater frisst so will ich keine zeit verlieren will noch ein wenig quinquillieren und lustig pfeifen wie zuvor der vogel scheint mir hat humor")
-
-func _process(delta: float) -> void:
+func change_state(next_state):
+	current_state = next_state
+	"""
 	match current_state:
 		State.READY:
-			pass
+			print("changed to ready")
+		State.READING:
+			print("changed to reading")
+		State.FINISHED:
+			print("changed to finished")
+	"""
+
+# State Machine:
+#
+# READY  ➡  READING
+#   ⬆        ⬇         
+#     FINISHED
+#
+# READY    ➡ READING : via display_dialogue() (tween starts animation)
+# READING  ➡ FINISHED : via on_tween_finished() (animation finished) 
+#                        or _process() (player skips animation)
+# FINISHED ➡ READY :
+
+
+func _ready() -> void:
+	hide_textbox()
+	"""
+	#print("starting state: ready")
+	var my_script = DialogueLibrary.load_json_file("res://scripts/test_script.json")
+	dialogue_queue = DialogueLibrary.extract_lines(my_script, "german")
+	"""
+
+func _process(_delta: float) -> void:
+	match current_state:
+		State.READY:
+			if !dialogue_queue.is_empty():
+				display_dialogue()
+			elif Input.is_action_just_pressed("ui_accept"):
+				hide_textbox()
 		State.READING:
 			if Input.is_action_just_pressed("ui_accept"):
 				label.visible_ratio = 1
-				tween.stop()
+				tween.kill()
 				change_state(State.FINISHED)
 		State.FINISHED:
 			if Input.is_action_just_pressed("ui_accept"):
 				change_state(State.READY)
-				hide_textbox()
-
+	
+func display_dialogue():
+	#set up next text
+	show_textbox()
+	var next_text = dialogue_queue.pop_front()
+	label.text = next_text
+	label.visible_ratio = 0
+	#configure state machine
+	change_state(State.READING)
+	#tween animation
+	var duration = next_text.length() * CHAR_READ_RATE
+	if tween: 
+		tween.kill()
+	tween = get_tree().create_tween()
+	#animation: visible_ration of lable is set from zero to 1 over the time of duration
+	#then emits signal "finished"
+	tween.tween_property(label, "visible_ratio", 1, duration).finished
+	#tween.connect("finished", on_tween_finished)
+	
 func hide_textbox():
 	label.text = ""
 	textbox_container.hide()
@@ -45,21 +93,4 @@ func show_textbox():
 func on_tween_finished():
 		change_state(State.FINISHED)
 	
-func add_text(next_text):
-	change_state(State.READING)
-	label.text = next_text
-	var duration = next_text.length() * CHAR_READ_RATE
-	show_textbox()
-	tween.tween_property(label, "visible_ratio", 1, duration)
-	tween.connect("finished", on_tween_finished)
-	
-func change_state(next_state):
-	current_state = next_state
-	match current_state:
-		State.READY:
-			print("changed to ready")
-		State.READING:
-			print("changed to reading")
-		State.FINISHED:
-			print("changed to finished")
 	
